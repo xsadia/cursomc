5c7075df440d3c31e5839138de1e6f6606a74468
xsadia <lipearthur@hotmail.com>
2021-02-08 14:36:37 -0300

    repository & service

--- src/main/java/com/fezin/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fezin/cursomc/resources/CategoriaResource.java
@@ -1,27 +1,26 @@
 package com.fezin.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fezin.cursomc.domain.Categoria;
+import com.fezin.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 	
-	@GetMapping
-	public ResponseEntity<List<Categoria>> listar() {
-		Categoria cat1 = new Categoria(1L, "Cursos");
-		Categoria cat2 = new Categoria(2L, "Hardware");
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Long id) {
 		
-		List<Categoria> list = new ArrayList<>();
-		list.add(cat1);
-		list.add(cat2);
-		return ResponseEntity.ok().body(list);
+		Categoria obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/fezin/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.fezin.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fezin.cursomc.domain.Categoria;
+import com.fezin.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Long id) {
+		Optional<Categoria> obj = repository.findById(id);
+		
+		return obj.orElse(null);
+	}
+}
