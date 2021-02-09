ae863e7bdb78f7d51bad665050b5c1cc9f34451d
xsadia <lipearthur@hotmail.com>
2021-02-09 14:54:51 -0300

    instanciacao dos dados mock

--- src/main/java/com/fezin/cursomc/CursomcApplication.java
+++ src/main/java/com/fezin/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.fezin.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.fezin.cursomc.domain.Categoria;
+import com.fezin.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		repository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
