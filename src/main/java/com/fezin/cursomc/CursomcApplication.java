8f86ba4f2a0ada3bd3130f661c4b0414a1228c06
xsadia <lipearthur@hotmail.com>
2021-02-10 15:50:07 -0300

    classes Estado e cidade e suas relações mapeadas no banco de dados

--- src/main/java/com/fezin/cursomc/CursomcApplication.java
+++ src/main/java/com/fezin/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fezin.cursomc.domain.Categoria;
+import com.fezin.cursomc.domain.Cidade;
+import com.fezin.cursomc.domain.Estado;
 import com.fezin.cursomc.domain.Produto;
 import com.fezin.cursomc.repositories.CategoriaRepository;
+import com.fezin.cursomc.repositories.CidadeRepository;
+import com.fezin.cursomc.repositories.EstadoRepository;
 import com.fezin.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -47,6 +57,16 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Santa Catarina");
+		
+		Cidade c1 = new Cidade(null, "Florianópolis", est2);	
+		Cidade c2 = new Cidade(null, "São Paulo", est1);
+		Cidade c3 = new Cidade(null, "Campinas", est1);
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
