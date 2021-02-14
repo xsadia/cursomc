eb1e00c186c280500cae41b0b9fd4ad31d30888a
xsadia <lipearthur@hotmail.com>
2021-02-14 13:54:52 -0300

    rota post categorias

--- src/main/java/com/fezin/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fezin/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.fezin.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fezin.cursomc.domain.Categoria;
 import com.fezin.cursomc.services.CategoriaService;
@@ -22,5 +27,13 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
