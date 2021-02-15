ded5590da6e564d23b354e1f47d4462e4975f6bb
xsadia <lipearthur@hotmail.com>
2021-02-15 17:50:43 -0300

    get, put, delete de clientes

--- src/main/java/com/fezin/cursomc/resources/ClienteResource.java
+++ src/main/java/com/fezin/cursomc/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package com.fezin.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fezin.cursomc.domain.Cliente;
+import com.fezin.cursomc.dto.ClienteDTO;
 import com.fezin.cursomc.services.ClienteService;
 
 @RestController
@@ -16,11 +27,50 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Cliente> find(@PathVariable Long id) {
 		
 		Cliente obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cliente> update(@PathVariable Long id,@Valid @RequestBody ClienteDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		
+		obj = service.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
+			) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/fezin/cursomc/services/ClienteService.java
+++ src/main/java/com/fezin/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.fezin.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fezin.cursomc.domain.Cliente;
+import com.fezin.cursomc.dto.ClienteDTO;
 import com.fezin.cursomc.repositories.ClienteRepository;
+import com.fezin.cursomc.services.exceptions.DataIntegrityException;
 import com.fezin.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,42 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repository.findAll(pageRequest);
+	}
+	
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	public void delete(Long id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		}
+		
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um cliente que possui entidades relacionadas."); 
+		}
+		
+	}
+
+	public Cliente update(Long id, Cliente obj) {
+		Cliente entity = find(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Cliente entity, Cliente obj) {
+		entity.setNome(obj.getNome());
+		entity.setEmail(obj.getEmail());
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 }
