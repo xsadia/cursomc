f624205aedcd42497e1b22c82517e51c66f7174e
xsadia <lipearthur@hotmail.com>
2021-02-22 19:40:55 -0300

    validacao email nao repetido update usuario

---
+++ src/main/java/com/fezin/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.fezin.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.fezin.cursomc.domain.Cliente;
+import com.fezin.cursomc.dto.ClienteDTO;
+import com.fezin.cursomc.repositories.ClienteRepository;
+import com.fezin.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	HttpServletRequest request;
+	
+	@Autowired
+	ClienteRepository clienteRepo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = clienteRepo.findByEmail(objDto.getEmail());
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
